050f0dddf4d930b6e1ecb2643e11e95c0e5d86e5
Firmiana <935170397@qq.com>
2019-11-04 10:52:00 +0800

    :art: edit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is a ReactCli-Base Demo ~
+        </p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload !
         </p>
         <a
           className="App-link"
